509761764e731462447e03ed4e56243ab1c6baa5
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-10-18 17:12:26 +0200

    Add site description

--- hell/_data/hell.js
+++ hell/_data/hell.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
   site_title: "HTMHell",
-  site_description: "A collection of bad practices in HTML found on real websites."
+  site_description: "A collection of bad practices in HTML, copied from real websites."
 };
