5da56560fc977d4fdd567bfe96901bcb828ccab4
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-10-22 08:03:17 +0200

    Fix og image path

--- hell/_data/hell.js
+++ hell/_data/hell.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
+  site_url: "https://htmhell.netlify.com",
   site_title: "HTMHell",
   site_description: "A collection of bad practices in HTML, copied from real websites."
 };
