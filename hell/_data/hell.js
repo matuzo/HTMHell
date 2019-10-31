363ce5a0a8bc7e43cc5a0ef95647121ed8be5cdf
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-10-31 06:36:17 +0100

    Link to twitter

--- hell/_data/hell.js
+++ hell/_data/hell.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: process.env.ELEVENTY_ENV,
   site_url: "https://htmhell.dev",
+  twitter_url: "https://twitter.com/htm_hell",
   site_title: "HTMHell",
   site_description: "A collection of bad practices in HTML, copied from real websites."
 };
