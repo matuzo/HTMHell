7900d64fa4eb497c67f759e16e61cde12b69cb14
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-11-02 10:50:13 +0100

    Add more snippets

--- hell/_data/snippets.js
+++ hell/_data/snippets.js
@@ -1,3 +1,5 @@
 module.exports = {
-  sr_only: "Unfortunately, there’s no native way of hiding content only visually.<br>This class makes sure that content is visually hidden but still accessible to screen reader users."
+  sr_only: "Unfortunately, there’s no native way of hiding content only visually.<br>This class makes sure that content is visually hidden but still accessible to screen reader users.",
+  native_semantics: "Do not change native semantics, unless you really have to",
+  button_click: "A click event on a `button` triggers on click and if the users presses the <kbd>Enter</kbd> or <kbd>Space</kbd> key"
 };
