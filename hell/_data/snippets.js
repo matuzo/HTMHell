92f11d3e8615695b7030837352e6c48517cf7074
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2019-11-29 08:29:43 +0100

    Add span button snippet

--- hell/_data/snippets.js
+++ hell/_data/snippets.js
@@ -2,5 +2,6 @@ module.exports = {
   sr_only: "Unfortunately, there’s no native way of hiding content only visually.<br>This class makes sure that content is visually hidden but still accessible to screen reader users.",
   native_semantics: "Do not change native semantics, unless you really have to.",
   button_click: "A click event on a `button` triggers on click and if the users presses the <kbd>Enter</kbd> or <kbd>Space</kbd> key.",
-  div_click: "A click event on a `div` triggers only on click."
+  div_click: "A click event on a `div` triggers only on click.",
+  span_click: "A click event on a `span` triggers only on click."
 };
