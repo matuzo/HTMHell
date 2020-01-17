7b71a36e8ec977237b23f30ff0cf968aa1badb99
Manuel Matuzovic <m.matuzovic@nousdigital.net>
2020-01-17 07:20:50 +0100

    Add new snippets
    - use button
    - negative tabindex

--- hell/_data/snippets.js
+++ hell/_data/snippets.js
@@ -3,5 +3,7 @@ module.exports = {
   native_semantics: "Do not change native semantics, unless you really have to.",
   button_click: "A click event on a `button` triggers on click and if the user presses the <kbd>Enter</kbd> or <kbd>Space</kbd> key.",
   div_click: "A click event on a `div` triggers only on click.",
-  span_click: "A click event on a `span` triggers only on click."
+  span_click: "A click event on a `span` triggers only on click.",
+  use_button: "If you need a button, use the `<button>` element.",
+  negative_tabindex: "A negative `tabindex` value means that the element is not accessible via keyboard, but it could be focused with Javascript"
 };
