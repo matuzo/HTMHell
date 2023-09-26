8e984ecb7cc06bee94e34cac065e731e364b300b
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-09-26 07:59:26 +0200

    Fixed broken images, updated text, adding author

--- hell/_data/authors.js
+++ hell/_data/authors.js
@@ -1,89 +1,94 @@
 module.exports = {
   authors: [
     {
-      "id": "mmatuzo",
-      "name": "Manuel",
-      "link": "https://www.matuzo.at/"
+      id: "mmatuzo",
+      name: "Manuel",
+      link: "https://www.matuzo.at/",
     },
     {
-      "id": "schepp",
-      "name": "Schepp",
-      "link": "https://twitter.com/derSchepp"
+      id: "schepp",
+      name: "Schepp",
+      link: "https://twitter.com/derSchepp",
     },
     {
-      "id": "hidde",
-      "name": "Hidde",
-      "link": "https://twitter.com/hdv"
+      id: "hidde",
+      name: "Hidde",
+      link: "https://twitter.com/hdv",
     },
     {
-      "id": "pepelsbey",
-      "name": "Vadim",
-      "link": "https://twitter.com/pepelsbey_"
+      id: "pepelsbey",
+      name: "Vadim",
+      link: "https://twitter.com/pepelsbey_",
     },
     {
-      "id": "smaerdian",
-      "name": "Sarah",
-      "link": "https://twitter.com/smaerdian"
+      id: "smaerdian",
+      name: "Sarah",
+      link: "https://twitter.com/smaerdian",
     },
     {
-      "id": "dirty-co.de",
-      "name": "Dirty-Co.de",
-      "link": "https://dirty-co.de"
+      id: "dirty-co.de",
+      name: "Dirty-Co.de",
+      link: "https://dirty-co.de",
     },
     {
-      "id": "svsven",
-      "name": "Sven",
-      "link": "https://github.com/svsven"
+      id: "svsven",
+      name: "Sven",
+      link: "https://github.com/svsven",
     },
     {
-      "id": "erik",
-      "name": "Erik",
-      "link": "https://twitter.com/erikkroes"
+      id: "erik",
+      name: "Erik",
+      link: "https://twitter.com/erikkroes",
     },
     {
-      "id": "odepax",
-      "name": "Odepax",
-      "link": "https://github.com/Odepax"
+      id: "odepax",
+      name: "Odepax",
+      link: "https://github.com/Odepax",
     },
     {
-      "id": "claire",
-      "name": "Claire",
-      "link": "https://twitter.com/abrightclearweb"
+      id: "claire",
+      name: "Claire",
+      link: "https://twitter.com/abrightclearweb",
     },
     {
-      "id": "spell",
-      "name": "Spell",
-      "link": "https://michaelspellacy.com/"
-    }, 
-    {
-      "id": "sime",
-      "name": "Šime",
-      "link": "https://twitter.com/simevidas"
+      id: "spell",
+      name: "Spell",
+      link: "https://michaelspellacy.com/",
     },
     {
-      "id": "vavroom",
-      "name": "vavroom",
-      "link": "https://twitter.com/vavroom"
+      id: "sime",
+      name: "Šime",
+      link: "https://twitter.com/simevidas",
     },
     {
-      "id": "christophfreyer",
-      "name": "Christoph",
-      "link": "https://www.christoph-freyer.at/"
+      id: "vavroom",
+      name: "vavroom",
+      link: "https://twitter.com/vavroom",
     },
     {
-      "id": "andreavaghi",
-      "name": "Andrea",
-      "link": "https://twitter.com/andreavaghi"
+      id: "christophfreyer",
+      name: "Christoph",
+      link: "https://www.christoph-freyer.at/",
     },
     {
-      "id": "huy_ngo",
-      "name": "Huy Ngo",
-      "link": "https://fosstodon.org/@huy_ngo"
+      id: "andreavaghi",
+      name: "Andrea",
+      link: "https://twitter.com/andreavaghi",
     },
     {
-      "id": "smellai",
-      "name": "Stefania",
-      "link": "https://github.com/smellai"
-    }
-  ]
+      id: "huy_ngo",
+      name: "Huy Ngo",
+      link: "https://fosstodon.org/@huy_ngo",
+    },
+    {
+      id: "smellai",
+      name: "Stefania",
+      link: "https://github.com/smellai",
+    },
+    {
+      id: "stevefrenzel",
+      name: "Steve Frenzel",
+      link: "https://stevefrenzel.dev",
+    },
+  ],
 };
